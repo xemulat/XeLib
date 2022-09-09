0897e1ba9257bfa26a2074703259c70fb944e5a4
xemulat <98595166+xemulat@users.noreply.github.com>
2022-09-09 21:50:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Xemulated',                   # Type in your name
   author_email = 'xemulated@tuta.io',      # Type in your E-Mail
   url = 'https://github.com/xemulat',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/xemulat/XeLib/archive/refs/tags/0.1.tar.gz',    # I explain this later on
   keywords = ['QOL', 'xemulated', 'downloader'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'colorama'],
