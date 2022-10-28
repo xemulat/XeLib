4b3621fa1096fb641b212c53748a638c4913b0a4
xemulat <98595166+xemulat@users.noreply.github.com>
2022-10-28 22:50:08 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'XeLib',
   packages = ['XeLib'],
-  version = '0.5',
+  version = '0.6',
   license='mit',
   description = "Xemulated's QOL Library",
   author = 'Xemulated',
   author_email = 'xemulated@tuta.io',
   url = 'https://github.com/xemulat',
-  download_url = 'https://github.com/xemulat/XeLib/archive/refs/tags/0.5.tar.gz',
+  download_url = 'https://github.com/xemulat/XeLib/archive/refs/tags/0.6.tar.gz',
   keywords = ['QOL', 'xemulated', 'downloader'],
   install_requires=['colorama', 'ping3'],
   classifiers=[
@@ -16,7 +16,7 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.10',
   ],
 )
